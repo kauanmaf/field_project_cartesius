5f8ceb3cfc83a021069c9b9479c9d034e898a339
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-09-06 15:38:45 -0300

    Agora as funções recebem apenas uma série com a data e a coluna do dado

--- strategy.py
+++ strategy.py
@@ -9,44 +9,22 @@ class Strategy():
     def run(self):
         pass
 
-    def moving_average(self, data_column, date_column, size):
-        # Número de dados
-        n_of_data = data_column.size
-        # Array para conter a média móvel
-        moving_average = np.zeros((n_of_data - size + 1))
+    # Função para calcular a média móvel exponencial dos dados
+    def moving_average(self, data: pd.Series, size: int) -> pd.Series:
+        average = data.ewm(span = size, adjust = False).mean()
+        return average
 
-        # Primeira média móvel calculada como aritmética
-        previous_mme = np.mean(data_column[0:size - 1])
-        # Salvando a primeira média
-        moving_average[0] = previous_mme
-
-        # Multiplicador da fórmula
-        alpha = 2/(size + 1)
-
-        # Para cada média móvel...
-        for i in range(1, n_of_data - size + 1):
-            # Calcula a nova média
-            new_mme = (data_column[size - 1 + i] - previous_mme) * alpha + previous_mme
-            # Salva no array
-            moving_average[i] = new_mme
-            # Atualiza a anterior
-            previous_mme = new_mme
-
-        # Adiciona a data à série
-        dated_average = pd.Series(moving_average, date_column[size - 1:])
-        
-        return dated_average
 
 class MA_crossover(Strategy):
-    def __init__(self, data, short_period, long_period) -> None:
+    def __init__(self, data: pd.Series, short_period: int, long_period: int) -> None:
         super().__init__()
         self.data = data
         self.short_period = short_period
         self.long_period = long_period
     
     def run(self):
-        long_average = self.moving_average(self.data["Adj Close"], self.long_period)
-        short_average = self.moving_average(self.data["Adj Close"], self.short_period)
+        long_average = self.moving_average(self.data, self.long_period)
+        short_average = self.moving_average(self.data, self.short_period)
         
         # Fazendo a interseção dos valores das médias de acordo com as datas
         long_average_aligned, short_average_aligned = long_average.align(short_average, join = "inner")
@@ -63,5 +41,3 @@ class MA_crossover(Strategy):
         diff[diff * short_average_var < 0] = 0
 
         return diff
-
-    
