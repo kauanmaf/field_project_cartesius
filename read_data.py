2631c84aae67a44c68e9b30749ac8e2d33adb80d
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-10-11 08:58:23 -0300

    Continuando o quintuplets

--- read_data.py
+++ read_data.py
@@ -3,16 +3,16 @@ import pandas as pd
 # Carregando os dados e ajustando o datetime
 tsla_data = pd.read_csv("data/TSLA.csv")
 tsla_data["Date"] = pd.to_datetime(tsla_data["Date"])
-tsla_data = pd.Series(tsla_data["Adj Close"].values, index = tsla_data["Date"])
+tsla_data.set_index("Date", inplace = True)
 
 azul_data = pd.read_csv("data/AZUL4.SA.csv")
 azul_data["Date"] = pd.to_datetime(azul_data["Date"])
-azul_data = pd.Series(azul_data["Adj Close"].values, index = azul_data["Date"])
+azul_data.set_index("Date", inplace = True)
 
 prio_data = pd.read_csv("data/PRIO3.SA.csv")
 prio_data["Date"] = pd.to_datetime(prio_data["Date"])
-prio_data = pd.Series(prio_data["Adj Close"].values, index = prio_data["Date"])
+prio_data.set_index("Date", inplace = True)
 
 viva_data = pd.read_csv("data/VIVA3.SA.csv")
 viva_data["Date"] = pd.to_datetime(viva_data["Date"])
-viva_data = pd.Series(viva_data["Adj Close"].values, index = viva_data["Date"])
+viva_data.set_index("Date", inplace = True)
