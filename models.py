57a7713fbf71ee85b9c61252ea5927e4a91a93c3
Pedro H. Coterli <pedrocoterli@gmail.com>
2024-11-01 15:50:51 -0300

    Começando a tunagem

---
+++ models.py
@@ -0,0 +1,112 @@
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+def mlp(data, y, hidden_layers=(100, 100, 100), activation='logistic', 
+        solver='adam', max_iter=500, random_state=42):
+    """
+    Treina um modelo de rede neural MLP e retorna as previsões e o relatório de classificação.
+
+    Parâmetros:
+    - data: DataFrame contendo os dados de entrada.
+    - hidden_layers: Tupla com o tamanho das camadas ocultas.
+    - activation: Função de ativação a ser usada.
+    - solver: Algoritmo de otimização a ser usado.
+    - max_iter: Número máximo de iterações.
+    - random_state: Semente para a geração de números aleatórios.
+
+    Retorna:
+    - y_pred_mlp: Previsões das classes no conjunto de teste.
+    - report: Relatório de classificação.
+    """
+    # Divide os dados em conjuntos de treinamento e teste
+    X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.2, random_state=42)
+
+    # Definindo a rede neural com múltiplas camadas
+    mlp = MLPClassifier(hidden_layer_sizes = hidden_layers,
+                        activation = activation,
+                        solver = solver,
+                        max_iter = max_iter,
+                        random_state = random_state)
+
+    # Treina a rede neural
+    mlp.fit(X_train, y_train)
+
+    # Faz previsões de classe
+    y_pred_mlp = mlp.predict(X_test)
+
+    # Exibe o relatório de classificação para o MLP
+    report = classification_report(y_test, y_pred_mlp)
+    print(report)
+
+    return mlp
+
+
+def random_forest(data, y, n_estimators=100, max_depth=None, random_state=42):
+    """
+    Treina um modelo de Random Forest e retorna as previsões e o relatório de classificação.
+
+    Parâmetros:
+    - data: DataFrame com as features e a variável alvo.
+    - target_column: Nome da coluna alvo no DataFrame.
+    - n_estimators: Número de árvores na floresta.
+    - max_depth: Profundidade máxima das árvores (None para ilimitado).
+    - random_state: Semente para a geração de números aleatórios.
+
+    Retorna:
+    - y_pred_rf: Previsões das classes no conjunto de teste.
+    - report: Relatório de classificação.
+    """
+    # Divide os dados em conjuntos de treinamento e teste
+    X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.2, random_state=42)
+
+    # Definindo o modelo Random Forest
+    rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, 
+                                 random_state=random_state)
+
+    # Treina o modelo Random Forest
+    rf.fit(X_train, y_train)
+
+    # Faz previsões de classe
+    y_pred_rf = rf.predict(X_test)
+
+    # Exibe o relatório de classificação para o Random Forest
+    report = classification_report(y_test, y_pred_rf)
+    print(report)
+
+    return rf
+
+
+def gradient_boosting(data, y, random_state=42):
+    """
+    Treina um modelo de Gradient Boosting e retorna as previsões e o relatório de classificação.
+
+    Parâmetros:
+    - data: DataFrame com as features e a variável alvo.
+    - target_column: Nome da coluna alvo no DataFrame.
+    - random_state: Semente para a geração de números aleatórios.
+
+    Retorna:
+    - y_pred_gb: Previsões das classes no conjunto de teste.
+    - report: Relatório de classificação.
+    """
+    # Divide os dados em conjuntos de treinamento e teste
+    X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.2, random_state=42)
+
+    # Definindo o modelo Gradient Boosting
+    gb_model = GradientBoostingClassifier(random_state=random_state)
+
+    # Treina o modelo Gradient Boosting
+    gb_model.fit(X_train, y_train)
+
+    # Faz previsões de classe
+    y_pred_gb = gb_model.predict(X_test)
+
+    # Exibe o relatório de classificação para o Gradient Boosting
+    report = classification_report(y_test, y_pred_gb)
+    print(report)
+
+    return gb_model
